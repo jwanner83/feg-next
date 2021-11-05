faa448dac387cb4ce66ef6ec87f1fce64fdda455
Jonas Wanner <jonas@wanner.com>
2021-11-06 00:52:45 +0100

    feat: display found predigten in component

--- pages/predigten.tsx
+++ pages/predigten.tsx
@@ -1,11 +1,34 @@
+import { dehydrate } from 'react-query'
+import { postEndpoint } from "../api/post/PostEndpoint"
+
 export default function Predigten () {
-    
+    const { isLoading, isError, data, error } = postEndpoint.useGetPredigtenRequest()
+
+    const predigten = data?.map(predigt => {
+        return <h1 key={predigt.id}>{predigt.title.rendered}</h1>
+    })
 
     return (
         <>
             <h1>Predigten</h1>
 
-            {}
+            {isLoading && <p>Is Loading</p>}
+
+            {data && (
+                <>
+                <p>data is filled</p>
+                {predigten}
+                </>
+            )}
         </>
     )
 }
+
+export async function getServerSideProps() {
+    const queryClient = await postEndpoint.prefetchPredigtenRequest(null)
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient)
+      }
+    }
+  }
