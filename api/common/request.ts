f488623e2166579150a323f155fed3b0dff4b77b
Jonas Wanner <jonas@wanner.com>
2021-11-06 00:52:16 +0100

    feat: add correct base url

--- api/common/request.ts
+++ api/common/request.ts
@@ -1,7 +1,7 @@
 import { fetcher } from './fetcher';
 import { ApiRequestOptions } from './request.types';
 
-const baseUrl = process.env.WORDPRESS_API_URL;
+const baseUrl = 'https://test.feg-gossau.ch/wp-json/wp/v2/';
 
 /**
  * Prepares the request received by an endpoint function to be fetched
