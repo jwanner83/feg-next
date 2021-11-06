b586878ef17d278d189da31e84a2c0592733e314
Jonas Wanner <jonas@wanner.com>
2021-11-06 15:26:22 +0100

    feat: adding tailwind config for fontsizes and grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,26 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
-      },
-      spacing: {
-        28: '7rem',
-      },
-      letterSpacing: {
-        tighter: '-.04em',
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
-      },
+    fontFamily: {
+      'serif': ['Playfair Display', 'serif']
     },
+    extend: {
+      gridTemplateColumns: {
+       'archive': '3fr 4fr',
+      }
+    }
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.4xl'), fontFamily: 'Playfair Display' },
+        'h2': { fontSize: theme('fontSize.2xl'), fontFamily: 'Playfair Display' },
+        'h3': { fontSize: theme('fontSize.lg'), fontFamily: 'Playfair Display' },
+      })
+    })
+  ]
 }
