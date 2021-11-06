1eaf0fa8fd5f51b6c5f60eb69d7709e64e00eb27
Jonas Wanner <jonas@wanner.com>
2021-11-06 15:23:26 +0100

    feat: implement get single post and fix types

--- api/post/PostEndpoint.ts
+++ api/post/PostEndpoint.ts
@@ -1,20 +1,35 @@
 import { GetContext } from "../base/Base.types";
 import { BaseEndpoint } from "../base/BaseEndpoint";
 import { apiRequest } from "../common/request";
-import { Predigt } from "./PostEndpoints.types";
+import { GetPostParams, Post } from "./PostEndpoints.types";
 
 class PostEndpoint extends BaseEndpoint<PostEndpoint> {
     key = 'post'
 
     getPredigtenRequest(
         context: GetContext<null>
-    ): Promise<Predigt[]> {
-        return apiRequest('predigten')
+    ): Promise<Post[]> {
+        return apiRequest('predigten', {
+            singleQueryParams: ['_embed']
+        })
     }
 
     useGetPredigtenRequest = this.createUseGetHook(this.getPredigtenRequest)
 
     prefetchPredigtenRequest = this.createGetPrefetch(this.getPredigtenRequest)
+
+    getPostRequest(
+        context: GetContext<GetPostParams>
+    ): Promise<Post[]> {
+        return apiRequest(context.params.type, {
+            queryParams: {
+                slug: context.params.slug
+            },
+            singleQueryParams: ['_embed']
+        })
+    }
+
+    useGetPostRequest = this.createUseGetHook(this.getPostRequest)
 }
 
 export const postEndpoint = new PostEndpoint()
