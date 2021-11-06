f025147138818d1e0f823fd93593e5d0793e60c5
Jonas Wanner <jonas@wanner.com>
2021-11-06 15:29:30 +0100

    feat: adding browser icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document () {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet" />        </Head>
         <body className="m-0">
+            <link rel="icon" href="/images/feg-logo.png" />
           <Main />
           <NextScript />
         </body>
