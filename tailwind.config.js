b7008e94d19f00b755524f30d83a274e0930b2fb
Jonas Wanner <jonas@wanner.com>
2021-11-06 15:46:43 +0100

    feat: move to src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     fontFamily: {
