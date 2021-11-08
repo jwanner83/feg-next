92991fcab19203593d21c529ed7a7c70906bf143
Jonas Wanner <jonas@wanner.com>
2021-11-08 12:08:50 +0100

    feat: adding correct playfair display font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css?family=Playfair+Display%3A400%2C700%7CLora%3A400%2C700%7COpen+Sans%3A300%2C400%2C600%2C700&subset=latin%2Clatin-ext"
           rel="stylesheet"
-        />{' '}
+        />
       </Head>
       <body className="m-0">
         <link rel="icon" href="/images/feg-logo.png" />
