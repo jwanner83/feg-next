3e90a7366b22e5840b3fb7387be4c488ce592aa9
Jonas Wanner <jonas.wanner@clavisit.com>
2021-11-08 15:55:58 +0100

    feat: integrate logrocket

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import { Html, Head, Main, NextScript } from 'next/document'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 
 export default function Document() {
+  if (typeof window !== 'undefined') {
+    LogRocket.init('aplimj/feg-next');
+    setupLogRocketReact(LogRocket);
+  }
+
   return (
     <Html>
       <Head>
