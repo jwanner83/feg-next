5d08e4040cf59456b93d81b47213b3137bd50ee7
Jonas Wanner <jonas@wanner.com>
2021-11-10 19:16:33 +0100

    feat: add content of page to index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import 'tailwindcss/tailwind.css'
-import '../styles/post.scss'
+import '@/styles/post.scss'
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { useEffect } from 'react'
 import LogRocket from 'logrocket'
 import setupLogRocketReact from 'logrocket-react'
-import CoreLayout from '../components/core/CoreLayout'
+import CoreLayout from '@/components/core/CoreLayout'
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      LogRocket.init('aplimj/feg-next');
-      setupLogRocketReact(LogRocket);
+      LogRocket.init('aplimj/feg-next')
+      setupLogRocketReact(LogRocket)
     }
   })
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
+import { pageEndpoint } from '@/api/page/PageEndpoint'
+import { Page } from '@/api/page/PageEndpoints.types'
 import Head from 'next/head'
 
-export default function Index() {
+type IndexParams = {
+  page: Page
+}
+
+export default function Index({ page }: IndexParams) {
   return (
     <>
       <Head>
@@ -10,6 +16,19 @@ export default function Index() {
       <div className="flex justify-center items-center py-56">
         <h1 className="font-bold md:text-6xl text-4xl">Willkommen</h1>
       </div>
+
+      <div dangerouslySetInnerHTML={{ __html: page?.content?.rendered }}></div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const page = await pageEndpoint.getPageRequest({
+    params: { slug: 'Willkommen' },
+    key: 'willkommen'
+  })
+  return {
+    props: { page: page?.[0] },
+    revalidate: 10
+  }
 }
