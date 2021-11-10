deb0079224d16f7549050df38804d09b223e47a6
Jonas Wanner <jonas@wanner.com>
2021-11-10 19:08:07 +0100

    feat: use new archvie components

--- src/pages/predigten.tsx
+++ src/pages/predigten.tsx
@@ -1,25 +1,17 @@
-import { dehydrate, QueryClient } from 'react-query'
-import { postEndpoint } from '../api/post/PostEndpoint'
-import ArchiveContainer from '../components/archive/ArchiveContainer'
-import ArchiveItem from '../components/archive/ArchiveItem'
+import { postEndpoint } from '@/api/post/PostEndpoint'
+import ArchiveItem from '@/components/archive/ArchiveItem'
 import Head from 'next/head'
-import { Post } from '../api/post/PostEndpoints.types'
+import { Post } from '@/api/post/PostEndpoints.types'
+import Archive from '@/components/archive/Archive'
 
 export default function Predigten({ posts }) {
-  const predigten = posts?.map((predigt: Post) => {
-    return <ArchiveItem key={predigt.id} item={predigt} base="predigten" />
-  })
-
   return (
     <>
       <Head>
         <title>Predigten - FEG Gossau</title>
       </Head>
-      <ArchiveContainer title="Predigten">
-        {posts && (
-          <div className="flex flex-col md:gap-8 gap-16">{predigten}</div>
-        )}
-      </ArchiveContainer>
+
+      <Archive title="Predigten" posts={posts} />
     </>
   )
 }
