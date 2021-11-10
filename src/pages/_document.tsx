5cb05c58b6ebfb5836df0bddd2dcce9e8513e97e
Jonas Wanner <jonas@wanner.com>
2021-11-10 19:09:00 +0100

    fix: move icon to correct position

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,13 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="icon" href="/images/feg-logo.png" />
         <link
           href="https://fonts.googleapis.com/css?family=Playfair+Display%3A400%2C700%7CLora%3A400%2C700%7COpen+Sans%3A300%2C400%2C600%2C700&subset=latin%2Clatin-ext&display=swap"
           rel="stylesheet"
         />
       </Head>
       <body className="m-0">
-        <link rel="icon" href="/images/feg-logo.png" />
         <Main />
         <NextScript />
       </body>
