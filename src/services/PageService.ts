525fce754d2a70b48780c48d631f1f9cc8c01897
Jonas Wanner <jonas@wanner.com>
2021-11-11 19:09:42 +0100

    feat: create page service

---
+++ src/services/PageService.ts
@@ -0,0 +1,19 @@
+import { Page, RawPage } from '@/api/endpoints/page/page.types'
+import { dateService } from './DateService'
+import { imageService } from './ImageService'
+
+class PageService {
+  getPost(raw: RawPage): Page {
+    return {
+      id: raw.id,
+      slug: raw.slug,
+      title: raw.title.rendered,
+      content: raw.content.rendered,
+      excerpt: raw.excerpt.rendered,
+      formattedDate: dateService.getFormattedDate(new Date(raw.date)),
+      image: imageService.getImage(raw._embedded)
+    }
+  }
+}
+
+export const pageService = new PageService()
