a9fc3adae22290bb03232046f83b3cf8d014d406
Jonas Wanner <jonas@wanner.com>
2021-11-11 21:01:44 +0100

    feat: add get pages request

--- src/api/endpoints/page/page.ts
+++ src/api/endpoints/page/page.ts
@@ -2,6 +2,20 @@ import { request } from '@/api/request'
 import { pageService } from '@/services/PageService'
 import { getPageParams, Page, RawPage } from './page.types'
 
+export async function getPages() {
+  const pages: RawPage[] = await request('pages', {
+    params: {
+      _embed: ''
+    }
+  })
+
+  if (pages && pages.length > 0) {
+    return pages.map((page) => pageService.getPage(page))
+  }
+
+  return null
+}
+
 export async function getPage({ slug }: getPageParams): Promise<Page> {
   const post: RawPage[] = await request('pages', {
     params: {
@@ -11,7 +25,7 @@ export async function getPage({ slug }: getPageParams): Promise<Page> {
   })
 
   if (post && post.length > 0) {
-    return pageService.getPost(post[0])
+    return pageService.getPage(post[0])
   }
 
   return null
