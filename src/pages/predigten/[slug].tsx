c648d0fbae99c669cbe4acf60b41478324d664a9
Jonas Wanner <jonas@wanner.com>
2021-11-11 18:45:13 +0100

    feat: implement new post api

--- src/pages/predigten/[slug].tsx
+++ src/pages/predigten/[slug].tsx
@@ -1,26 +1,17 @@
-import { useRouter } from 'next/router'
-import { postEndpoint } from '@/api/post/PostEndpoint'
 import Head from 'next/head'
 import PostComponent from '@/components/post/Post'
-import { Post } from '@/api/post/PostEndpoints.types'
+import { Post } from '@/api/endpoints/post.types'
+import { getPost, getPosts } from '@/api/endpoints/post'
 
 type PostParams = {
   post: Post
 }
 
 export default function PredigtenPost({ post }: PostParams) {
-  const router = useRouter()
-
-  const image =
-    post?._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url
-
-  const rawDate = new Date(post?.date)
-  const date = `${rawDate?.getDate()}.${rawDate?.getMonth()}.${rawDate.getFullYear()}`
-
   return (
     <>
       <Head>
-        <title>{post?.title.rendered} - FEG Gossau</title>
+        <title>{post?.title} - FEG Gossau</title>
       </Head>
 
       <PostComponent post={post} />
@@ -29,23 +20,21 @@ export default function PredigtenPost({ post }: PostParams) {
 }
 
 export async function getStaticProps({ params }) {
-  const post = await postEndpoint.getPostRequest({
-    params: { slug: params.slug, type: 'predigten' },
-    key: params.slug
-  })
+  const post = await getPost({ slug: params.slug, type: 'predigten' })
 
   return {
     props: {
-      post: post?.[0]
+      post
     },
     revalidate: 10
   }
 }
 
 export async function getStaticPaths() {
-  const predigten = await postEndpoint.getPredigtenMinimalRequest()
+  const posts = await getPosts({ type: 'predigten' })
+
   return {
-    paths: predigten.map((predigt: Post) => `/predigten/${predigt.slug}`) || [],
+    paths: posts.map((post) => `/predigten/${post.slug}`) || [],
     fallback: true
   }
 }
