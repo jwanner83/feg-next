b9ababfbb06e3093deda7ef83d4b4940c62f7af8
Jonas Wanner <jonas@wanner.com>
2021-11-11 20:28:52 +0100

    feat: enable mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,15 @@ export default function Index({ page }: IndexParams) {
         <title>FEG Gossau - Die Freie Evangelische Gemeinde in Gossau</title>
       </Head>
 
-      <div className="h-index flex items-center">
-        <h1 className="font-bold text-huge absolute text-gray-200 -ml-12">
+      <div className="h-index flex items-center relative md:static">
+        <h1 className="font-bold text-huge absolute -ml-12 z-10 opacity-30 text-black md:text-gray-200">
           {page.title}
         </h1>
-        <h1 className="font-bold text-5xl relative mt-3">{page.title}</h1>
+        <h1 className="font-bold text-5xl relative mt-3 z-10 ml-6 text-white md:text-black">
+          {page.title}
+        </h1>
 
-        <div className="absolute h-index-image w-2/4 right-0">
+        <div className="absolute h-index w-full z-0 md:z-10 md:h-index-image md:w-2/4 md:right-0">
           <Image
             className="object-cover"
             src={page.image.large}
