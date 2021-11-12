9aca7c403efd2bceeb18c23b7e3af431605eb29e
Jonas Wanner <jonas@wanner.com>
2021-11-12 20:48:58 +0100

    feat: adding generic type to the request method

--- src/api/request.ts
+++ src/api/request.ts
@@ -22,14 +22,20 @@ function prepare(endpoint: string, options: options) {
   }
 }
 
-export async function request(endpoint: string, options: options) {
+export async function request<ResponseData>(
+  endpoint: string,
+  options: options
+): Promise<undefined | { data: ResponseData; headers: Headers }> {
   const prepared = prepare(endpoint, options)
 
   try {
     const response = await fetch(prepared.url, prepared.options)
     const data = await response.json()
 
-    return data
+    return {
+      data,
+      headers: new Headers(response.headers)
+    }
   } catch (error) {
     console.error(`request to ${endpoint} failed`, error)
   }
