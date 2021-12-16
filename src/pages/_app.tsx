df606405aa0847554660aa9df928574c464f57a9
Jonas Wanner <jonas@wanner.com>
2021-12-16 21:11:36 +0100

    feat: remove unused theme setter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,19 +23,6 @@ export default function App({ Component, pageProps }) {
       LogRocket.init('aplimj/feg-next')
       setupLogRocketReact(LogRocket)
     }
-
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    // always set dark mode
-    document.documentElement.classList.add('dark')
   })
 
   fetch()
