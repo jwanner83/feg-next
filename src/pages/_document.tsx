6a679b32e6314ef46e16b771a35f686b2bcde6c5
Jonas Wanner <jonas@wanner.com>
2021-12-19 15:56:59 +0100

    fix: add language to html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="de">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
