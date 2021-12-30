141d6fc7aa69c9936f5cfbfe67c3ea67b83eed10
Jonas Wanner <jonas@wanner.com>
2021-12-30 18:09:00 +0100

    fix: comment out image

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -6,7 +6,7 @@ export default function Badge() {
         <svg fill="#fff"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12l-11-8v6h-13v4h13v6z"/></svg>
       </div>
       <div className="absolute z-10 h-full w-full left-0 bg-gradient-to-r from-black to-transparent" />
-      <img className="absolute h-full w-full top-0 left-0 object-cover" src="https://feg-gossau.ch/wp-content/uploads/2020/09/laughing-scaled.jpg" />
+      {/*<img className="absolute h-full w-full top-0 left-0 object-cover" src="https://feg-gossau.ch/wp-content/uploads/2020/09/laughing-scaled.jpg" />*/}
     </div>
   </>
 }
