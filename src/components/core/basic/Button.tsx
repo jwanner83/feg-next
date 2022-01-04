b8610d38cdc9e2032850aee7e67691489c263df6
Jonas Wanner <jonas.wanner@clavisit.com>
2022-01-04 09:15:27 +0100

    feat: improving button to allow custom background colors

--- src/components/core/basic/Button.tsx
+++ src/components/core/basic/Button.tsx
@@ -1,16 +1,24 @@
-import { MouseEventHandler, ReactNode } from 'react'
+import { MouseEventHandler, ReactNode, useEffect, useState } from 'react'
 
 type ButtonParams = {
   children: ReactNode
   className?: string
+  background?: string
   onClick?: () => MouseEventHandler
 }
 
-export default function Button({ children, className, onClick }: ButtonParams) {
+export default function Button({ children, className, background, onClick }: ButtonParams) {
+  const [backgroundClasses, setBackgroundClasses] = useState('bg-gray-200 hover:bg-black hover:bg-black')
+
+  useEffect(() => {
+    if (background) {
+      setBackgroundClasses(background)
+    }  
+  }, [background])
   return (
     <button
       onClick={onClick}
-      className={`py-3 px-4 uppercase font-bold bg-gray-200 ${className} transition-colors hover:bg-black hover:text-white`}
+      className={`py-3 px-4 uppercase font-bold transition-colors ${backgroundClasses} ${className}`}
     >
       {children}
     </button>
