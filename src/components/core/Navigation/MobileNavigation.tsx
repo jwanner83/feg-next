4bc9e6b364ababfe7ac6d3cd563a2084ef0ad4a0
Jonas Wanner <jonas.wanner@clavisit.com>
2022-01-04 13:41:02 +0100

    feat: include theme switcher in header

--- src/components/core/Navigation/MobileNavigation.tsx
+++ src/components/core/Navigation/MobileNavigation.tsx
@@ -3,6 +3,7 @@ import CoreImage from '@/components/core/CoreImage'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import CoreTheme from '../CoreTheme'
 import { NavigationParams } from './navigation.types'
 
 export default function MobileNavigation({ menus }: NavigationParams) {
@@ -13,7 +14,7 @@ export default function MobileNavigation({ menus }: NavigationParams) {
   const navigationItems = menus?.map((menu: Menu) => (
     <div key={menu.ID}>
       <Link href={`/${menu.slug}`} passHref={true}>
-        <a className="font-bold text-5xl">{menu.title}</a>
+        <a className="font-bold text-5xl dark:text-white">{menu.title}</a>
       </Link>
     </div>
   ))
@@ -45,15 +46,12 @@ export default function MobileNavigation({ menus }: NavigationParams) {
       <div
         className={`${
           state ? 'h-screen' : 'h-0'
-        } w-screen fixed top-0 left-0 bg-white z-30 flex flex-col gap-2 overflow-y-hidden transition-height duration-300`}
+        } w-screen fixed top-0 left-0 bg-white dark:bg-dark z-30 flex flex-col gap-2 overflow-y-hidden transition-height duration-300`}
       >
         <div className="pt-32 px-8 z-50">{navigationItems}</div>
 
-        <div className="absolute h-full w-full z-40 opacity-25">
-          <CoreImage
-            title="background"
-            image="https://test.feg-gossau.ch/wp-content/uploads/2021/12/hannah-busing-Zyx1bK9mqmA-unsplash-scaled.jpg"
-          />
+        <div className="absolute w-full h-36 bottom-0 flex justify-center">
+          <CoreTheme />
         </div>
       </div>
     </div>
