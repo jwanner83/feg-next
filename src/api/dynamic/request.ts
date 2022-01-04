840f00adfc9f118f4e7afb9a3bdc4cb8a928dc08
Jonas Wanner <jonas.wanner@clavisit.com>
2022-01-04 09:20:13 +0100

    fix: fetcher method param types

--- src/api/dynamic/request.ts
+++ src/api/dynamic/request.ts
@@ -1,1 +1,8 @@
-export const fetcher = (...args: any[]) => fetch(...args).then(res => res.json())
+export const fetcher = async (
+    input: RequestInfo,
+    init: RequestInit,
+    ...args: any[]
+  ) => {
+    const res = await fetch(input, init);
+    return res.json();
+  };
