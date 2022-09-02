30b526f89585d120b13f4253f6b33a3e562b1e85
Jonas Wanner <jonas@wanner.com>
2022-09-02 22:56:24 +0200

    feat: add Notion renderer to slug

--- src/pages/notion/[slug].tsx
+++ src/pages/notion/[slug].tsx
@@ -1,31 +1,37 @@
 import Head from 'next/head'
 import { NotionNormalizedPost } from '@/api/static/endpoints/post/normalized.post.types'
-import { getPage } from '@/api/static/notion/client'
+import { getBlocks, getPage } from '@/api/static/notion/client'
+import Notion from '@/components/notion/Notion'
+import { NotionBlockType } from '@/services/notion/types/notion.types'
 
 type PostParams = {
-  post: NotionNormalizedPost
+  post: NotionNormalizedPost,
+  blocks: NotionBlockType[]
 }
 
-export default function PredigtenPost({ post }: PostParams) {
+export default function PredigtenPost({ post, blocks }: PostParams) {
   return (
     <>
       <Head>
         <title>{post?.title} - FEG Gossau</title>
       </Head>
 
-      {post?.slug}
+      <Notion blocks={blocks} />
     </>
   )
 }
 
 export async function getStaticProps({ params }) {
   const post = await getPage(params.slug)
+  const blocks = await getBlocks(post.id)
 
   console.log('post', post)
+  console.log('blocks', blocks)
 
   return {
     props: {
-      post
+      post,
+      blocks
     },
     revalidate: 10
   }
